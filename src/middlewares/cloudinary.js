37bcebc438add3d5dbce2267a2da2ab98d17125c
ShuklaNishant <71431028+niscollect@users.noreply.github.com>
2025-07-04 07:40:14 +0530

    Update cloudinary.js

--- src/middlewares/cloudinary.js
+++ src/middlewares/cloudinary.js
@@ -3,11 +3,11 @@ import { v2 as cloudinary } from "cloudinary";
 // Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   "djxvscnkn", // ! process.env.CLOUDINARY_CLOUD_NAME
+//  , // ! process.env.CLOUDINARY_CLOUD_NAME
   api_key: process.env.CLOUDINARY_API_KEY,
-//   "128617887955785", // ! process.env.CLOUDINARY_API_KEY
+// , // ! process.env.CLOUDINARY_API_KEY
   api_secret: process.env.CLOUDINARY_API_SECRET,
-//   "JP6mfaOMk_fUK5SiGpBTP3KMXB4", // ! process.env.CLOUDINARY_API_SECRET
+// , // ! process.env.CLOUDINARY_API_SECRET
 });
 
 export const uploadOnCloudinaryURL = async (localFilePath) => {
